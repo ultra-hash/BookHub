b5bdf98db2800ff0a997b6801850e9c9911b84c6
veerabhadraswamy <jarviscommand3@gmail.com>
2023-07-20 09:35:35 +0000

    Added - BookShelvesView and BookDetailsView Components

---
+++ src/components/BookDetailsView/index.js
@@ -0,0 +1,19 @@
+import {Component} from 'react'
+
+import Footer from '../Footer'
+import './index.css'
+
+class BookDetailsView extends Component {
+  state = {}
+
+  render() {
+    return (
+      <div>
+        <h1>Book Details View</h1>
+        <Footer />
+      </div>
+    )
+  }
+}
+
+export default BookDetailsView
---
+++ src/components/BookShelvesView/index.js
@@ -0,0 +1,19 @@
+import {Component} from 'react'
+
+import Footer from '../Footer'
+import './index.css'
+
+class BookShelvesView extends Component {
+  state = {}
+
+  render() {
+    return (
+      <div>
+        <h1>Book Shelves View</h1>
+        <Footer />
+      </div>
+    )
+  }
+}
+
+export default BookShelvesView
