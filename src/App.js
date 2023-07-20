2e49e5dc9b9dcce8a083d4a930cb132144040212
veerabhadraswamy <jarviscommand3@gmail.com>
2023-07-20 09:39:06 +0000

    Configured - Routes to BookShelvesView and BookDetailsView

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import ProtectedRoute from './components/ProtectedRoute'
 import LoginView from './components/LoginView'
 import HomeView from './components/HomeView'
 import NotFoundView from './components/NotFoundView'
+import BookShelvesView from './components/BookShelvesView'
+import BookDetailsView from './components/BookDetailsView'
 import './App.css'
 
 // use the below bookshelvesList for rendering read status of book items in Bookshelves Route
@@ -34,6 +36,8 @@ const bookshelvesList = [
 const App = () => (
   <Switch>
     <ProtectedRoute exact path="/" component={HomeView} />
+    <ProtectedRoute exact path="/shelf" component={BookShelvesView} />
+    <ProtectedRoute exact path="/books/:id" component={BookDetailsView} />
     <Route exact path="/login" component={LoginView} />
     <Route exact path="/not-found" component={NotFoundView} />
     <Redirect to="not-found" />
