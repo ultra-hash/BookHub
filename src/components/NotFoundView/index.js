812cf1cb4ce0082985ab5717abecd562bcc36182
veerabhadraswamy <jarviscommand3@gmail.com>
2023-07-20 08:15:52 +0000

    Added - Redirect Functionality inside NotFoundView

--- src/components/NotFoundView/index.js
+++ src/components/NotFoundView/index.js
@@ -1,21 +1,31 @@
 import './index.css'
 
-const NotFoundView = () => (
-  <div className="NotFoundView-OuterContainer">
-    <img
-      className="NotFoundView-NotFoundImage"
-      src="https://res.cloudinary.com/dxcascpje/image/upload/f_auto,q_auto/v1/BookHub/Page-Not-Found"
-      alt="not found"
-    />
-    <h1 className="NotFoundView-Heading">Page Not Found</h1>
-    <p className="NotFoundView-Description">
-      we are sorry, the page you requested could not be found. Please go back to
-      the homepage.
-    </p>
-    <button type="button" className="NotFoundView-Button">
-      Go Back to Home
-    </button>
-  </div>
-)
+const NotFoundView = props => {
+  const redirectToHome = () => {
+    const {history} = props
+    history.replace('/')
+  }
 
+  return (
+    <div className="NotFoundView-OuterContainer">
+      <img
+        className="NotFoundView-NotFoundImage"
+        src="https://res.cloudinary.com/dxcascpje/image/upload/f_auto,q_auto/v1/BookHub/Page-Not-Found"
+        alt="not found"
+      />
+      <h1 className="NotFoundView-Heading">Page Not Found</h1>
+      <p className="NotFoundView-Description">
+        we are sorry, the page you requested could not be found. Please go back
+        to the homepage.
+      </p>
+      <button
+        type="button"
+        className="NotFoundView-Button"
+        onClick={redirectToHome}
+      >
+        Go Back to Home
+      </button>
+    </div>
+  )
+}
 export default NotFoundView
