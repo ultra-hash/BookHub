851dde02601fb58dc77d9ddec6bcdcb418fe8728
veerabhadraswamy <jarviscommand3@gmail.com>
2023-08-08 06:05:12 +0000

    Bug Fix - Visuals fix to Header component and navbar toggle function

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,20 +1,56 @@
 import {Component} from 'react'
 import {Link} from 'react-router-dom'
+import {RiCloseCircleFill} from 'react-icons/ri'
+import {GiHamburgerMenu} from 'react-icons/gi'
 import './index.css'
 
 class Header extends Component {
-  state = {}
+  state = {showMenu: false}
+
+  toggleMenu = () => {
+    this.setState(prevState => ({
+      showMenu: !prevState.showMenu,
+    }))
+  }
 
   render() {
+    const {showMenu} = this.state
+
     return (
       <nav>
-        <div className="Header-section">
-          <div className="Header-mobile-section">
+        <div className="Header-section-main">
+          <div className="Header-section">
             <img
               className="Header-websiteLogo"
               src="https://res.cloudinary.com/dxcascpje/image/upload/f_auto,q_auto/v1/BookHub/logo"
               alt="website logo"
             />
+            <ul className="Header-UnorderedList Header-show-above-md">
+              <li>
+                <Link to="/" className="Header-Link">
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link to="/shelf" className="Header-Link">
+                  BookShelf
+                </Link>
+              </li>
+              <li>
+                <button type="button" className="Header-Logout-Btn">
+                  Logout
+                </button>
+              </li>
+            </ul>
+            <GiHamburgerMenu
+              size={16}
+              className="Header-show-below-md"
+              onClick={this.toggleMenu}
+            />
+          </div>
+        </div>
+        {showMenu && (
+          <div className="Header-section Header-show-below-md">
             <ul className="Header-UnorderedList">
               <li>
                 <Link to="/">Home</Link>
@@ -23,24 +59,16 @@ class Header extends Component {
                 <Link to="/shelf">BookShelf</Link>
               </li>
               <li>
-                <button type="button">Logout</button>
+                <button type="button" className="Header-Logout-Btn">
+                  Logout
+                </button>
+              </li>
+              <li>
+                <RiCloseCircleFill size={24} onClick={this.toggleMenu} />
               </li>
             </ul>
           </div>
-        </div>
-        <div className="Header-mobile-section">
-          <ul className="Header-UnorderedList">
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/shelf">BookShelf</Link>
-            </li>
-            <li>
-              <button type="button">Logout</button>
-            </li>
-          </ul>
-        </div>
+        )}
       </nav>
     )
   }
