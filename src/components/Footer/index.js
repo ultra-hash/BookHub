e2e67c8382cc534f23236271bbddc020fe6faf03
veerabhadra swamy uppu <jarviscommand3@gmail.com>
2024-01-24 08:35:21 +0000

    implement Footer component

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,29 @@
 import {Component} from 'react'
+import {FaGoogle, FaInstagram, FaTwitter, FaYoutube} from 'react-icons/fa'
 
 import './index.css'
 
 class Footer extends Component {
   render() {
-    return <h1>Footer</h1>
+    return (
+      <div className="footer-container">
+        <div className="footer-icons-container">
+          <li>
+            <FaGoogle className="footer-icon" />
+          </li>
+          <li>
+            <FaTwitter className="footer-icon" />
+          </li>
+          <li>
+            <FaInstagram className="footer-icon" />
+          </li>
+          <li>
+            <FaYoutube className="footer-icon" />
+          </li>
+        </div>
+        <p className="footer-contact-us">Contact Us</p>
+      </div>
+    )
   }
 }
 
