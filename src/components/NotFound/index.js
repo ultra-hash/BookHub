9c9ec35298f40015feb605f0520b8c1a6eb59a69
veerabhadra swamy uppu <jarviscommand3@gmail.com>
2024-01-24 10:31:34 +0000

    implement NotFound page component

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,10 +1,29 @@
 import {Component} from 'react'
+import {Link} from 'react-router-dom'
 
 import './index.css'
 
 class NotFound extends Component {
   render() {
-    return <h1>NotFound</h1>
+    return (
+      <div className="not-found-page">
+        <img
+          className="not-found-image"
+          src="https://res.cloudinary.com/dxcascpje/image/upload/f_auto,q_auto/v1/BookHub/Page-Not-Found"
+          alt="not found"
+        />
+        <h1 className="not-found-heading">Page Not Found</h1>
+        <p className="not-found-message">
+          we are sorry, the page you requested could not be found. Please go
+          back to the homepage.
+        </p>
+        <Link to="/" className="not-found-goto-home-link">
+          <button type="button" className="not-found-goto-home-button">
+            Go Back To Home
+          </button>
+        </Link>
+      </div>
+    )
   }
 }
 
